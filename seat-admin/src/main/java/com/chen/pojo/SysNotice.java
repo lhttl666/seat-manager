1fa54f64340cf2044c5ed30ce3c24e1ae1a85836
Administrator <910326532@qq.com>
2021-02-09 22:26:35 +0800

    实现反馈页面意见箱的显示和模糊查询

--- seat-admin/src/main/java/com/chen/pojo/SysNotice.java
+++ seat-admin/src/main/java/com/chen/pojo/SysNotice.java
@@ -7,10 +7,11 @@ import java.util.Date;
 
 @Data
 public class SysNotice implements Serializable {
-    private static final long serialVersionUID = 6647732552202996770L;
+    private static final long serialVersionUID = -7366197418540581823L;
     private Integer id;
     private String title;
     private String content;
+    private Integer type;
     private Date createdTime;
     private Date modifiedTime;
     private String createdUser;
